bc6183270066fb8051f62dbd4b9a8bb743c9fc2f
unknown <jahkaivanwalters1@gmail.com>
2024-07-22 13:12:37 -0400

    Added the addOne file

---
+++ addOne.py
@@ -0,0 +1,10 @@
+### Adds one to user number
+# LOGIC: added = user_number + 1
+# Components: 
+# function (params, user_number)
+# return added
+
+def addOne(user_number):
+    added = user_number + 1
+    return added
+print(addOne(10))
