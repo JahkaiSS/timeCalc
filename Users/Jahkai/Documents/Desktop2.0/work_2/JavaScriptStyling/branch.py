a6ac4ecd50c652e58d71cb773a67652d36a2f201
unknown <jahkaivanwalters1@gmail.com>
2024-07-22 14:01:24 -0400

    created game

---
+++ Users/Jahkai/Documents/Desktop2.0/work_2/JavaScriptStyling/branch.py
@@ -0,0 +1,20 @@
+import pygame
+from sys import *
+
+pygame.init()
+pygame.display.init()
+
+screen = pygame.display.set_mode((600,600))
+pygame.display.set_caption('branching')
+
+clock = pygame.time.Clock()
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.fill((100,101,109))
+    pygame.display.update()
+    clock.tick(60)
